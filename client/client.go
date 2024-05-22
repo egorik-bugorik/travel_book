438d9a80bb091809ff157be04ded4e7ff5b2fcc7
egorik <gori@.ru>
2024-05-22 23:01:45 +0300

    presleeping_workout

--- client/client.go
+++ client/client.go
@@ -4,7 +4,10 @@ import (
 	"context"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+	"io"
 	"log"
+	"math/rand"
+	"time"
 	"travel_book/travelbook"
 )
 
@@ -17,14 +20,76 @@ func main() {
 	}
 	client := travelbook.NewTravelBookClient(conn)
 
-	feature, err := client.GetFeature(context.Background(), &travelbook.Point{
-		Latitude:  406421967,
-		Longitude: -747727624,
+	featureByPoint(client, &travelbook.Point{
+		Latitude:  407838351,
+		Longitude: -746143763,
 	})
+
+	traversePoint(client)
+	featureInsideRectangle(client, &travelbook.Rectangle{
+		Lo: &travelbook.Point{
+			Latitude:  407838000,
+			Longitude: -746143000,
+		},
+		Hi: &travelbook.Point{
+			Latitude:  409838000,
+			Longitude: -736143000,
+		},
+	})
+
+}
+
+func traverseRoute(client travelbook.TravelBookClient) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+
+	stream, err := client.RecordRoute(ctx)
 	if err != nil {
-		log.Fatalf("Error while get feature for point ::: %v", err)
+		log.Fatalf("Error while traversing route features with RECT ::: %v", err)
 
 	}
-	log.Println(feature.Name)
+	defer cancel()
 
+}
+
+func featureInsideRectangle(client travelbook.TravelBookClient, rectangle *travelbook.Rectangle) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+	stream, err := client.ListFeature(ctx, rectangle)
+	if err != nil {
+		log.Fatalf("Error while getting features with RECT ::: %v", err)
+
+	}
+gori:
+	for {
+		feature, err := stream.Recv()
+		if err != nil {
+			if err == io.EOF {
+				break gori
+			}
+			log.Fatalf("Error while getting features with RECT ::: %v", err)
+
+		}
+		log.Printf("\n\n\t :::   Feature found -> %v   :::", feature.Name)
+	}
+
+}
+
+func featureByPoint(client travelbook.TravelBookClient, p *travelbook.Point) {
+
+	ctx := context.Background()
+	feature, err := client.GetFeature(ctx, p)
+	if err != nil {
+		log.Fatalf("Error while getting feature by point!!! %v", err)
+	}
+	log.Printf("\n:::    Feature  :::    %v", feature.Name)
+}
+
+func randomPoint() *travelbook.Point {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	lat := (r.Intn(180) - 90) * 1e7
+	lon := (r.Intn(360) - 180) * 1e7
+	return &travelbook.Point{
+		Latitude:  int32(lat),
+		Longitude: int32(lon),
+	}
 }
